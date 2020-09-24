86331f2acc60772d4904fcf42c8c70da376b78f0
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2020-09-24 18:29:01 +0000

    add: mock api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createServer, Model, Factory } from "miragejs"
+
+createServer({
+  models: {
+    article: Model
+  },
+
+  factories: {
+    article: Factory.extend({
+      title: 'Vivamus euismod a tellus eget interdum. Aenean ac.',
+      content: 'Aliquam vulputate mi in vulputate aliquam. Mauris ultrices vel felis eget tempus. Morbi a est at lacus malesuada ultrices ac quis turpis. Curabitur ante metus, malesuada eget neque eu, ornare suscipit ligula. Aliquam suscipit cursus eros, ut tincidunt nulla laoreet a. Donec aliquam urna vel pellentesque sodales.'
+    })
+  },
+
+  seeds(server) {
+    server.createList('article', 3);
+  },
+
+  routes() {
+    this.get("/api/articles", (schema) => {
+      return schema.articles.all();
+    });
+
+    this.get("/api/articles/:id", (schema, request) => {
+      let id = request.params.id;
+      return schema.find('article', id);
+    });
+
+    this.post("/api/articles", (schema, request) => {
+      let attrs = JSON.parse(request.requestBody);
+      return schema.articles.create(attrs);
+    });
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
