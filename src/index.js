935fda4884fe83da43de5cdabb4a7fed68bb3aef
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2021-02-15 19:12:06 +0000

    add: random article factory

--- src/index.js
+++ src/index.js
@@ -3,9 +3,44 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createServer, Model, Factory, Response } from "miragejs"
 
-import img from './assets/img.jpg';
+import img1 from './assets/1.jpg';
+import img2 from './assets/2.jpg';
+import img3 from './assets/3.jpg';
 
 const articlesPerPage = 5
+
+const imgs = [
+  img1,
+  img2,
+  img3
+]
+
+const titles = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  'Cras malesuada ligula erat, non egestas risus congue sit amet.',
+  'Vivamus euismod a tellus eget interdum. Aenean ac.'
+]
+
+const contents = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi semper quam et ultricies porta. Suspendisse semper pulvinar justo at maximus. Etiam a ex eu libero laoreet molestie. Nam tristique iaculis eros, vel efficitur urna lobortis in. Praesent consectetur quam quis nisi pellentesque vulputate. Aliquam vitae nibh libero. Morbi consequat ultricies enim quis tristique. Duis at odio non eros ornare volutpat et sed libero. Sed at turpis odio.',
+  'Cras malesuada ligula erat, non egestas risus congue sit amet. Vestibulum pretium non odio sit amet vehicula. Nunc lobortis tristique sapien, ac condimentum lorem interdum eget. Aenean imperdiet felis finibus metus aliquam euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sodales, ipsum faucibus consequat tincidunt, enim leo posuere neque, sed auctor nisi velit sit amet nisl.',
+  'Aliquam vulputate mi in vulputate aliquam. Mauris ultrices vel felis eget tempus. Morbi a est at lacus malesuada ultrices ac quis turpis. Curabitur ante metus, malesuada eget neque eu, ornare suscipit ligula. Aliquam suscipit cursus eros, ut tincidunt nulla laoreet a. Donec aliquam urna vel pellentesque sodales.'
+]
+
+function randomImg() {
+  let index = Math.floor(Math.random() * 3)
+  return imgs[index]
+}
+
+function randomTitle() {
+  let index = Math.floor(Math.random() * 3)
+  return titles[index]
+}
+
+function randomContent() {
+  let index = Math.floor(Math.random() * 3)
+  return contents[index]
+}
 
 createServer({
   models: {
@@ -14,9 +49,9 @@ createServer({
 
   factories: {
     article: Factory.extend({
-      image: img,
-      title: 'Vivamus euismod a tellus eget interdum. Aenean ac.',
-      content: 'Aliquam vulputate mi in vulputate aliquam. Mauris ultrices vel felis eget tempus. Morbi a est at lacus malesuada ultrices ac quis turpis. Curabitur ante metus, malesuada eget neque eu, ornare suscipit ligula. Aliquam suscipit cursus eros, ut tincidunt nulla laoreet a. Donec aliquam urna vel pellentesque sodales.'
+      image: () => randomImg(),
+      title: () => randomTitle(),
+      content: () => randomContent()
     })
   },
 
@@ -67,7 +102,7 @@ createServer({
 
     this.post("/api/articles", (schema, request) => {
       let attrs = request.requestBody;
-      return schema.articles.create({...attrs, image: img});
+      return schema.articles.create({...attrs, image: randomImg()});
     });
 
     this.post("/api/login", (schema, request) => {
